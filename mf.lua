763574a18a57e55b0189fb7ef17d6f92ce2bc5f7
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-07 03:42:25 -0700

    Update mf.lua

--- mf.lua
+++ mf.lua
@@ -1,30 +1,28 @@
-    component = require "component"
-    g = component.glasses 
-    args = {...}
- 
-    xs =args[1]
-    ys =args[2]
-    zs =args[3]
- 
-    r = args[4]
- 
-    g.removeAll()
- 
- 
-    function check(x,y,z,r)
-     return  (((x-xs)*(x-xs)) + ((y-ys)*(y-ys)) + ((z-zs)*(z-zs))) <= (r*r)
-    end
- 
-    for x=xs-r, xs+r do
-      for y = xs-r, xs+r do
-        for z = zs-r, zs+r do
-          if check(x,y,z,r) and not check(x,y,z,r-1) then
-            c = g.addCube3D()
-            c.set3DPos(x,y,z)
-            c.setScale(tonumber(args[5]))
-            c.setAlpha(0.4)
-          end
-          os.sleep(0)
-         end
+local component = require("component")
+local g = component.glasses
+local args = {...}
+
+local xs = tonumber(args[1])
+local ys = tonumber(args[2])
+local zs = tonumber(args[3])
+local r = tonumber(args[4])
+
+g.removeAll()
+
+local function check(x, y, z, r)
+  return ((x - xs) * (x - xs)) + ((y - ys) * (y - ys)) + ((z - zs) * (z - zs)) <= (r * r)
+end
+
+for x = xs - r, xs + r do
+  for y = ys - r, ys + r do
+    for z = zs - r, zs + r do
+      if check(x, y, z, r) and not check(x, y, z, r - 1) then
+        local c = g.addCube3D()
+        c.set3DPos(x, y, z)
+        c.setScale(tonumber(args[5]))
+        c.setAlpha(0.4)
       end
+      os.sleep(0)
     end
+  end
+end
