c144c522cb37becd0d92e52df35eb6dd0af92424
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-07 03:53:21 -0700

    Update mf.lua

--- mf.lua
+++ mf.lua
@@ -1,18 +1,5 @@
 local component = require("component")
 local g = component.glasses
-local args = {...}
-
-local xs = tonumber(args[1])
-local ys = tonumber(args[2])
-local zs = tonumber(args[3])
-local r = tonumber(args[4])
-
-g.removeAll()
-
--- Define the check function
-local function check(x, y, z, r)
-  return ((x - xs) * (x - xs)) + ((y - ys) * (y - ys)) + ((z - zs) * (z - zs)) <= (r * r)
-end
 
 cube = component.glasses.addCube3D()
 cube.addColor(50, 50, 50)
