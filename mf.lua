d7933f7b4695cf0a15fc9040a2f1c86849dd0bab
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-07 03:51:17 -0700

    Update mf.lua

--- mf.lua
+++ mf.lua
@@ -14,16 +14,6 @@ local function check(x, y, z, r)
   return ((x - xs) * (x - xs)) + ((y - ys) * (y - ys)) + ((z - zs) * (z - zs)) <= (r * r)
 end
 
-for x = xs - r, xs + r do
-  for y = ys - r, ys + r do
-    for z = zs - r, zs + r do
-      if check(x, y, z, r) and not check(x, y, z, r - 1) then
-        local c = g.addCube3D()
-        c.set3DPos(x, y, z)
-        c.setScale(tonumber(args[5]))
-        c.setAlpha(0.4)
-      end
-      os.sleep(0)
-    end
-  end
-end
+cube = component.glasses.addCube3D()
+cube.addColor(50, 50, 50)
+cube.addScale(2, 2, 2)
